ac5d3b732fa9c32ae366d005b73f163cc290534b
Lakshmikanth <72095624+Lakshmikanth-472@users.noreply.github.com>
2022-11-07 12:38:51 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args)
+    {
+        int firstNum=25;//first variable
+        int secondNum=50;//second variable
+        int result=firstNum+secondNum;//result 
+        System.out.println(result);
+    }
+}
