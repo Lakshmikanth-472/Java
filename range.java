2d6594ad482918ed3a0e5831670ae4c7d5511e37
Lakshmikanth <72095624+Lakshmikanth-472@users.noreply.github.com>
2022-11-08 18:27:38 +0530

    Add files via upload

---
+++ range.java
@@ -0,0 +1,29 @@
+public class range {
+    
+    public  static void main(String[]args){
+        int myIntValue=Integer.MIN_VALUE;
+        int myMaxVakue=Integer.MAX_VALUE;
+        System.out.println(myIntValue);//range of positive numbers
+        System.out.println(myMaxVakue);//range of negative numbers
+        //Integer is a wrapper class which is used to find range of data types
+        //overflow whn +ve range is exceeded and underflow when -ve range is exceeded
+        byte minValue=Byte.MIN_VALUE;
+        byte maxValue=Byte.MAX_VALUE;
+        System.out.println(minValue);
+        System.out.println(maxValue);
+        short minValueNum=Short.MIN_VALUE;
+        short maxValueNum=Short.MAX_VALUE;
+        System.out.println(minValueNum);
+        System.out.println(maxValueNum);
+      long myMinLongValue=Long.MIN_VALUE;
+      long myMaxLongValue=Long.MAX_VALUE;
+      System.out.println(myMinLongValue);
+      System.out.println(myMaxLongValue);
+
+
+
+
+
+
+    }
+}
